d554f4a920078597fe530c3177fabf2afd55c4db
cod-ME <jstbckup1@outlook.com>
2024-06-25 21:33:29 +0200

    jun_end update

---
+++ sys_config/.cache/wal/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P00A000B
+                 \e]P1B26EC2
+                 \e]P2C47BC2
+                 \e]P3F56BFB
+                 \e]P4DF8CB9
+                 \e]P5D789C1
+                 \e]P6EB97C1
+                 \e]P7f4c8d7
+                 \e]P8aa8c96
+                 \e]P9B26EC2
+                 \e]PAC47BC2
+                 \e]PBF56BFB
+                 \e]PCDF8CB9
+                 \e]PDD789C1
+                 \e]PEEB97C1
+                 \e]PFf4c8d7
+                 \ec'
