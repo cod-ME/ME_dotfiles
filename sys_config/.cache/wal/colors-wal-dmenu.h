7d30a29f2c54741d68a18be2aeff009082998872
cod-ME <149729391+cod-ME@users.noreply.github.com>
2025-12-05 22:54:53 +0000

    Delete sys_config directory

--- sys_config/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f4c8d7", "#0A000B" },
-	[SchemeSel] = { "#f4c8d7", "#B26EC2" },
-	[SchemeOut] = { "#f4c8d7", "#EB97C1" },
-};
